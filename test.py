9f0ac68b64642b6e90393ec8313290474ca21b69
Artem <webstermaster777@gmail.com>
2020-02-03 19:44:11 +0300

    TripletTorch_v1

---
+++ test.py
@@ -0,0 +1,5 @@
+from triplettorch import HardNegativeTripletMiner
+from triplettorch import AllTripletMiner
+from torch.utils.data import DataLoader
+from triplettorch import TripletDataset
+print("vse ok")
