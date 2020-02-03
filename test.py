51e384a54913e4fa469f311b00fffb1c1dd3a0a9
WEBSTERMASTER777 <38701834+WEBSTERMASTER777@users.noreply.github.com>
2020-02-03 23:11:33 +0300

    Delete test.py

--- test.py
+++
@@ -1,5 +0,0 @@
-from triplettorch import HardNegativeTripletMiner
-from triplettorch import AllTripletMiner
-from torch.utils.data import DataLoader
-from triplettorch import TripletDataset
-print("vse ok")
